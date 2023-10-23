53a56732ff4e610d2f55e74398e5d56112585ca5
Hank Liu <hankliu@coolbitx.com>
2023-10-23 18:00:20 +0800

    add tagged hash

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 java -jar ${DIR}/gp.jar -delete 436f6f6c57616c6c657450524f -delete 436f6f6c57616c6c6574 -r "Identiv uTrust 3700 F CL Reader 2"
 if [ "$1" == "1" ]; then
   echo "install parameter 80"
-  java -jar ${DIR}/gp.jar -install ${DIR}/bin/coolbitx/javacard/coolbitx.cap -params 80 -r "Identiv uTrust 3700 F CL Reader 2" -default
+  java -jar ${DIR}/gp.jar -install ${DIR}/bin/coolbitx/javacard/coolbitx.cap -params c0 -r "Identiv uTrust 3700 F CL Reader 2" -default
 else
   echo "install parameter null"
   java -jar ${DIR}/gp.jar -install ${DIR}/bin/coolbitx/javacard/coolbitx.cap -r "Identiv uTrust 3700 F CL Reader 2" -default
