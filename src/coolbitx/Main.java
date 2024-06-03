3a4b126b0c5c9b779da84c07f2fc5e4d883e70d4
Hank Liu <hankliu@coolbitx.com>
2024-06-03 13:10:55 +0800

    format code

--- src/coolbitx/Main.java
+++ src/coolbitx/Main.java
@@ -21,8 +21,7 @@ import javacardx.apdu.ExtendedLength;
  */
 public class Main extends Applet implements AppletEvent, ExtendedLength {
 
-	private static final short ver = 337
-			;
+	private static final short ver = 337;
 
 	private static boolean isInit = false;
 
